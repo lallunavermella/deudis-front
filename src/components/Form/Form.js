b0c272e61ddf31f1920940b177ebbc9cbe75022a
Silvia Martinez <sil@silvia.com>
2022-05-21 19:26:39 +0200

    Added value and changData and onSubmit methods

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,33 +1,57 @@
+import { useState } from "react";
+
 const Form = () => {
+  const blankFields = {
+    calerons: "",
+    deudora: "",
+    pobres: "",
+  };
+  const [formData, setFormData] = useState(blankFields);
+
+  const changeData = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
   return (
     <>
-      <form autoComplete="off">
+      <form autoComplete="off" onSubmit={onSubmit}>
         <label htmlFor="calerons">
           {" "}
-          Calerons: <input id="calerons" type="text" />
+          Calerons:{" "}
+          <input
+            id="calerons"
+            type="text"
+            value={formData.calerons}
+            onChange={changeData}
+          />
         </label>
         <label htmlFor="deudora">
           {" "}
           Deudora:
-          <select id="deudora">
-            <option>Alba</option>
-            <option>Giulia</option>
-            <option>Nuria LL.</option>
-            <option>Nuria G.</option>
-            <option>Silvia</option>
+          <select id="deudora" value={formData.deudora} onChange={changeData}>
+            <option value="Alba">Alba</option>
+            <option value={"Giulia"}>Giulia</option>
+            <option value={"Nuria LL."}>Nuria LL.</option>
+            <option value={"Nuria G."}>Nuria G.</option>
+            <option value={"Silvia"}>Silvia</option>
           </select>
         </label>
         <label htmlFor="pobres">
           {" "}
           Les Pobres:
-          <select id="pobres">
-            <option>Alba</option>
-            <option>Giulia</option>
-            <option>Nuria LL.</option>
-            <option>Nuria G.</option>
-            <option>Silvia</option>
+          <select id="pobres" value={formData.pobres} onChange={changeData}>
+            <option value="Alba">Alba</option>
+            <option value={"Giulia"}>Giulia</option>
+            <option value={"Nuria LL."}>Nuria LL.</option>
+            <option value={"Nuria G."}>Nuria G.</option>
+            <option value={"Silvia"}>Silvia</option>
           </select>
         </label>
+        <input type="submit" />
       </form>
     </>
   );
