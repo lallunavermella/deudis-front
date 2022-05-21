c87e3faa9aa8b9423fbc43fb2d81c1f30f2524ef
Silvia Martinez <sil@silvia.com>
2022-05-21 18:51:54 +0200

    Added options at form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,32 @@
 const Form = () => {
   return (
     <>
-      <form>
-        <label>
-          <input></input>
+      <form autoComplete="off">
+        <label htmlFor="calerons">
+          {" "}
+          Calerons: <input id="calerons" type="text" />
+        </label>
+        <label htmlFor="deudora">
+          {" "}
+          Deudora:
+          <select id="deudora">
+            <option>Alba</option>
+            <option>Giulia</option>
+            <option>Nuria LL.</option>
+            <option>Nuria G.</option>
+            <option>Silvia</option>
+          </select>
+        </label>
+        <label htmlFor="pobres">
+          {" "}
+          Les Pobres:
+          <select id="pobres">
+            <option>Alba</option>
+            <option>Giulia</option>
+            <option>Nuria LL.</option>
+            <option>Nuria G.</option>
+            <option>Silvia</option>
+          </select>
         </label>
       </form>
     </>
