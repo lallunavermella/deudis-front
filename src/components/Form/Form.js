958eb3d9c9e30107d6813d3ad71248c9309a9cf9
Silvia Martinez <sil@silvia.com>
2022-05-21 20:08:01 +0200

    Updated select form with componentSelect

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Select from "react-select";
+import chroma from "chroma-js";
 
 const Form = () => {
   const blankFields = {
@@ -6,6 +8,68 @@ const Form = () => {
     deudora: "",
     pobres: "",
   };
+
+  const data = [
+    { value: "Alba", label: "Alba", color: "#00B8D9" },
+    { value: "Giulia", label: "Giulia", color: "#0052CC" },
+    { value: "Nuria G.", label: "Nuria G.", color: "#5243AA" },
+    { value: "Nuria Ll.", label: "Nuria Ll.", color: "#FF5630" },
+    { value: "Silvia", label: "Silvia", color: "#FF8B00" },
+  ];
+
+  const colourOptions = {
+    control: (styles) => ({ ...styles, backgroundColor: "white" }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      const color = chroma(data.color);
+      return {
+        ...styles,
+        backgroundColor: isDisabled
+          ? undefined
+          : isSelected
+          ? data.color
+          : isFocused
+          ? color.alpha(0.1).css()
+          : undefined,
+        color: isDisabled
+          ? "#ccc"
+          : isSelected
+          ? chroma.contrast(color, "white") > 2
+            ? "white"
+            : "black"
+          : data.color,
+        cursor: isDisabled ? "not-allowed" : "default",
+
+        ":active": {
+          ...styles[":active"],
+          backgroundColor: !isDisabled
+            ? isSelected
+              ? data.color
+              : color.alpha(0.3).css()
+            : undefined,
+        },
+      };
+    },
+    multiValue: (styles, { data }) => {
+      const color = chroma(data.color);
+      return {
+        ...styles,
+        backgroundColor: color.alpha(0.1).css(),
+      };
+    },
+    multiValueLabel: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+    }),
+    multiValueRemove: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+      ":hover": {
+        backgroundColor: data.color,
+        color: "white",
+      },
+    }),
+  };
+
   const [formData, setFormData] = useState(blankFields);
 
   const changeData = (event) => {
@@ -32,24 +96,21 @@ const Form = () => {
         <label htmlFor="deudora">
           {" "}
           Deudora:
-          <select id="deudora" value={formData.deudora} onChange={changeData}>
-            <option value="Alba">Alba</option>
-            <option value={"Giulia"}>Giulia</option>
-            <option value={"Nuria LL."}>Nuria LL.</option>
-            <option value={"Nuria G."}>Nuria G.</option>
-            <option value={"Silvia"}>Silvia</option>
-          </select>
+          <Select
+            closeMenuOnSelect={true}
+            options={data}
+            styles={colourOptions}
+          ></Select>
         </label>
         <label htmlFor="pobres">
           {" "}
           Les Pobres:
-          <select id="pobres" value={formData.pobres} onChange={changeData}>
-            <option value="Alba">Alba</option>
-            <option value={"Giulia"}>Giulia</option>
-            <option value={"Nuria LL."}>Nuria LL.</option>
-            <option value={"Nuria G."}>Nuria G.</option>
-            <option value={"Silvia"}>Silvia</option>
-          </select>
+          <Select
+            closeMenuOnSelect={false}
+            options={data}
+            isMulti
+            styles={colourOptions}
+          ></Select>
         </label>
         <input type="submit" />
       </form>
